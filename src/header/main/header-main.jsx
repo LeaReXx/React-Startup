98345df1a053e3cb64e50d764001267e0c708bca
Sepehr Aghapour <dev.sepehr@outlook.com>
2023-03-20 19:47:59 +0330

    some style was changed in `header-main`

--- src/header/main/header-main.jsx
+++ src/header/main/header-main.jsx
@@ -2,35 +2,39 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function HeaderMain() {
   return (
-    <div className="flex flex-col pt-28 lg:flex-row lg:justify-between">
+    <div className="flex flex-col pt-28 lg:flex-row lg:justify-between lg:items-center">
       <div className="md:text-left max-w-md mx-auto md:mx-0">
         <h4 className="text-[#3BCBB0] bg-[#3acbb01a] py-2 px-3 rounded-full tracking-[.15em] font-medium inline">
           STARTUP
         </h4>
-        <h1 className="text-4xl md:text-5xl lg:text-6xl my-4 font-semibold leading-tight">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl my-4 font-semibold leading-tight text-[#10101B]">
           Build Amazing Startup Business Together
         </h1>
 
-        <p className="font-normal text-base">
+        <p className="font-normal text-lg py-1 lg:py-3">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio delectus
           eligendi totam culpa. Hic laboriosam, quis libero cumque iure
           assumenda.
         </p>
         <div className="flex flex-col my-3 gap-4 md:flex-row">
-          <button className="bg-[#1C2639] text-white py-3 rounded font-medium md:px-8">
+          <button className="bg-[#1C2639] text-white py-3 rounded font-medium md:px-8 smooth-trans hover:bg-[#3BCBB0]">
             Invest Now
             <FontAwesomeIcon
               icon="fa-solid fa-arrow-right-long"
               className="px-1"
             />
           </button>
-          <button className="border py-3 rounded font-medium md:px-4">
+          <button className="border py-3 rounded font-medium md:px-4 smooth-trans hover:bg-[#3BCBB0] hover:text-white">
             Learn More
           </button>
         </div>
       </div>
       <div className="p-5 my-2 lg:max-w-2xl 2xl:max-w-3xl">
-        <img src="./img/startup-banner.png" alt="Startup banner" className="w-full" />
+        <img
+          src="./img/startup-banner.png"
+          alt="Startup banner"
+          className="w-full"
+        />
       </div>
     </div>
   );
